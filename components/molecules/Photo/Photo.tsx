bcee503476b1ad7f8007bd87dad6526fe31069fd
Thiago Silva Ferreira <thiagosfsjc@gmail.com>
2021-10-10 08:02:58 -0300

    Fix object-fit into safari

--- components/molecules/Photo/Photo.tsx
+++ components/molecules/Photo/Photo.tsx
@@ -14,7 +14,7 @@ export const Photo: React.FC<Props> = function ({ data }) {
       <LazyImage
         src={data.url}
         alt={data.title}
-        className="h-full object-cover"
+        className="h-full object-cover min-h-full w-full"
       />
       <div className="absolute bottom-0 left-0 right-0 top-0 opacity-0 hover:opacity-50 transition-all duration-300 flex items-end">
         <div className="text-xs flex justify-between p-4 pt-24 bg-gradient-to-t from-black to-transparent w-full">
